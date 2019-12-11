b15ce214df11c44063d25613736071c2f8a9975f
KKevin07 <wweiwenw@qq.com>
2019-12-11 18:29:20 +0800

    比较基础的活动创建

---
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bn1=(Button)findViewById(R.id.button1);
+        Button bn2=(Button)findViewById(R.id.button4);
+        bn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main3Activity.class);
+                intent.putExtra("name","ww");
+                intent.putExtra("age",19);
+                intent.putExtra("tel",134);
+                startActivityForResult(intent,1);
+            }
+        });
+        bn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("name","ww");
+                intent.putExtra("age",19);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+    protected void onActivityResult(int req,int res,Intent a){
+        //重写
+        super.onActivityResult(req, res, a);
+        if(req==0&&res==0) {
+            String date = a.getStringExtra("result1");
+            Toast.makeText(MainActivity.this, date, Toast.LENGTH_SHORT).show();
+        }
+        if(req==1&&res==1){
+                String date1=a.getStringExtra("result2");
+                Toast.makeText(MainActivity.this,date1,Toast.LENGTH_SHORT).show();
+            }
+            //结束
+    }
+}
